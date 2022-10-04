a104faca0fd0f5ffe80a17ca0546a9961ed21f17
Raphael G <raphaelgal@gmail.com>
2022-10-04 13:40:18 +0300

    add security config

--- src/main/java/com/example/ecommerceproject/EcommerceProjectApplication.java
+++ src/main/java/com/example/ecommerceproject/EcommerceProjectApplication.java
@@ -2,12 +2,24 @@ package com.example.ecommerceproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
+@EnableJpaRepositories("com.example.ecommerceproject.model.persistence.repositories")
+@EntityScan("com.example.ecommerceproject.model.persistence")
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class EcommerceProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceProjectApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
